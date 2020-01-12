808ac9817c08c48c2e955f726305e16ff1547ad4
badrooo <badreddine.yacine99@gmail.com>
2020-01-12 18:53:14 +0000

    connection to local DB

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 // Uncaught Exception (that accurs in synchronous code)
@@ -11,6 +12,14 @@ const app = require('./app.js')
 dotenv.config({path: './config.env'});
 
 const port = process.env.PORT || 3000;
+const DB = process.env.DATABASE_LOCAL;
+
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+}).then(con => console.log('Connection to DB'));
 
 const server = app.listen(port, () => {
   console.log('The server is listening...');
