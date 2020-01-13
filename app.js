2db6bee737ea45a382dc09a127ab532c72bfad00
badrooo <badreddine.yacine99@gmail.com>
2020-01-13 20:35:54 +0000

    register, login, logout

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const ErrorResponse = require('./utils/errorResponse');
 const globalErrorHandler = require('./controllers/errorController');
+const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
   next(new ErrorResponse(`${req.originalUrl} not found in the server`, 404));
